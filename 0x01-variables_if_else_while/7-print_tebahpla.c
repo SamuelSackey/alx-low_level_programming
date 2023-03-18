75e7224f0646420398f4ec3d577b51dc1453ba9d
Samuel Sackey <smlscky@gmail.com>
2023-03-18 02:37:46 +0000

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 25; i > -1; i--)
+		putchar(str[i]);
+
+	putchar('\n');
+
+	return (0);
+}

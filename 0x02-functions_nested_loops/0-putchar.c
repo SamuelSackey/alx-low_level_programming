56d385efe8e70e6e25321d25e0fd4da05d18a7b4
Samuel Sackey <smlscky@gmail.com>
2023-03-22 02:09:11 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	char *str = "_putchar\n";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (0);
+}

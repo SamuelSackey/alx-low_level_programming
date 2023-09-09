2621a136b654bac89a92985f0ea0b1b502cc727b
Samuel Sackey <smlscky@gmail.com>
2023-09-09 02:38:45 +0000

    variables tasks complete

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				if ((i < j) && (j < k))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (i != 55)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

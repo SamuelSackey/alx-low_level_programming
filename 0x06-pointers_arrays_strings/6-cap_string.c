2ed67c0eb99d38eb086ce6408f496da0bfd4fe9d
Samuel Sackey <smlscky@gmail.com>
2023-09-21 03:07:34 +0000

    pointers 2 tasks complete

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be transformed
+ * Return: char*
+ */
+char *cap_string(char *s)
+{
+	int i, j, cap = 1;
+
+	char *sep = " \t\n,;.!?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (cap && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] + 'A' - 'a';
+		}
+
+		cap = 0;
+
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				cap = 1;
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
